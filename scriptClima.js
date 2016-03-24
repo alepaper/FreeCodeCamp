ac4fc685ab8165623298d1f1945fc5dd06bd2bc4
alepaper <hectoralejandropatinoperez@gmail.com>
2016-03-24 11:46:44 -0500

    Se modificó la página del clima, agregando iconos del viento (dirección y velocidad) y del estado del clima de acuerdo con el id

--- scriptClima.js
+++ scriptClima.js
@@ -1,6 +1,6 @@
 var temp = 0;
-var icon='<i class="wi wi-celsius"></i>';
-// var icon='&deg;';
+// var icon='<i class="wi wi-celsius"></i>';
+var icon='&deg;';
 var latitude=6.222527299999999;
 var longitude=-75.5740975;
 var geo_options = {
@@ -59,7 +59,19 @@ var geo_options = {
         })
         .done(function(data) {
             temp=data.main.temp;
-            $('#temp').html(' ' + temp + icon);
+            $('#temp').html(' ' + temp.toFixed(2) + icon);
+            $('#toc').prop("class","wi wi-celsius");
+            $('#tof').prop("class","wi wi-fahrenheit");
+            $('#pic1').html('<i class="pic wi wi-owm-' + data.weather[0].id + '"></i>');
+            $('#pic2').html('<i class="pic wi wi-windy"></i>');
+            $('#pic1').append('<h2>' + data.weather[0].description + '</h2>');
+            $('#pic2').append('<h2>Wind</h2>');
+            $('#pic2').append('<h3>Speed: ' + data.wind.speed + ' <i class="wi wi-wind-beaufort-'
+                + Math.round(data.wind.speed) + '"></i></h3>');
+            $('#pic2').append('<h3>Direction: ' + data.wind.deg +
+             '&deg; <i class="wi wi-wind from-' + data.wind.deg +
+             '-deg"></i></h3>');
+
         });
     }
 
@@ -68,12 +80,12 @@ var geo_options = {
              $('#tof').css('opacity','1');
              $('#toc').css('opacity','0.5');
              temp=temp*9/5+32;
-             icon='<i class="wi wi-fahrenheit"></i>';
+             // icon='<i class="wi wi-fahrenheit"></i>';
          } else {
              $('#toc').css('opacity','1');
              $('#tof').css('opacity','0.5');
              temp=(temp-32)*5/9;
-             icon='<i class="wi wi-celsius"></i>';
+             // icon='<i class="wi wi-celsius"></i>';
          }
          $('#temp').html(' ' + temp.toFixed(2) + icon);
     }
